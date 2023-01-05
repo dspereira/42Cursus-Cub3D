2af4b331653bc72943a8e85b665ea3a2370ab110
Diogo13Antunes <diogo@LAPTOP-NTMTRPGI>
2023-01-05 17:17:54 +0000

    Corrections on RGB format checker.

--- MapChecker/sources/get_file_textures_utils.c
+++ MapChecker/sources/get_file_textures_utils.c
@@ -1,10 +1,8 @@
 #include "../includes/header.h"
 
 static int  get_number(char *src, int *index);
+static int check_rgb_elemts(char *src);
 
-/*
-    int bgrValue = (b << 16) + (g << 8) + r
-*/
 int get_rgb_colour(char *src, int *tex_count, int actual_colour)
 {
     int r;
@@ -14,6 +12,8 @@ int get_rgb_colour(char *src, int *tex_count, int actual_colour)
 
     if (actual_colour != -1)
         return (actual_colour);
+    if (!check_rgb_elemts(src))
+        return (-1);
     index = 2;
     while ((src[index] >= 9 && src[index] <= 13) || src[index] == 32)
         index++;
@@ -42,7 +42,31 @@ static int  get_number(char *src, int *index)
     }
     if (src[i] == ',')
         *index = i + 1;
+    else
+        *index = i;
     if (nbr > 255 || nbr < 0)
         nbr = -1;
     return (nbr);
+}
+
+static int check_rgb_elemts(char *src)
+{
+    int i;
+    int amount_nbrs;
+
+    i = 2;
+    amount_nbrs = 0;
+    while ((src[i] >= 9 && src[i] <= 13) || src[i] == 32)
+        i++;
+    while (src[i] != '\n')
+    {
+        if ((src[i] < '0' || src[i] > '9') && src[i] != ',')
+            return (FALSE);
+        if (src[i] == ',')
+            amount_nbrs++;
+        i++;
+    }
+    if (amount_nbrs != 2)
+        return (FALSE);
+    return (TRUE);
 }
