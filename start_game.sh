b4bc7f81a6305618f969de525df05a4255048bc4
Diogo13Antunes <diogo@LAPTOP-NTMTRPGI>
2023-01-17 12:15:12 +0000

    Reajusting the start_game Script

--- start_game.sh
+++ start_game.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo $#
-
-if [$# -eq 1];
-then
-	echo "Invalid Number of Args" && exit 1
-else
-	make re
-	echo "------------------------------------------------"
-	./cub3D $@
-	echo "------------------------------------------------"
-	make fclean
-fi
+make re
+./cub3D $@
+make fclean
