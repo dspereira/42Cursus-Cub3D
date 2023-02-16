34b9b8686e90cff9a7d6340e5afff23543457aa9
Diogo andre Candeias antunes <dcandeia@c2r10s8.42lisboa.com>
2023-02-16 10:51:44 +0000

    Updated minimap

--- sources/minimap.c
+++ sources/minimap.c
@@ -3,31 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dcandeia < dcandeia@student.42lisboa.co    +#+  +:+       +#+        */
+/*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:44:56 by dcandeia          #+#    #+#             */
-/*   Updated: 2023/02/15 17:19:23 by dcandeia         ###   ########.fr       */
+/*   Updated: 2023/02/16 10:38:55 by dcandeia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-static void render_map(t_img img, char **map);
-static void render_background(t_img img);
-static void render_player(t_img img, t_player player, char **map);
-static void render_player_circle(t_img img, t_player player, char **map);
-static void draw_line_1(t_img img, t_pos init, t_pos end, char **map, float angle);
+static void	render_map(t_img img, char **map);
+static void	render_background(t_img img);
+static void	render_player(t_img img, t_player player, char **map);
+static void	render_player_circle(t_img img, t_player player, char **map);
+static void	draw_line_1(t_img img, t_pos init, t_pos end, char **map, float angle);
 
-void minimap_render(t_img img, t_player player, char **map)
+void	minimap_render(t_img img, t_player player, char **map)
 {
 	//render_background(img);
 	//render_map(img, map);
-    
 	render_player_circle(img, player, map);
 	//render_player_circle(img, player, map);
 }
 
-static void render_map(t_img img, char **map)
+static void	render_map(t_img img, char **map)
 {
 	t_pos	map_pos;
 	t_pos	win_pos;
@@ -42,7 +41,7 @@ static void render_map(t_img img, char **map)
 			//win_pos.y = (map_pos.y * MINIMAP_SQUARE_SIZE) + (WIN_HEIGHT / 2) - (MINIMAP_SQUARE_SIZE * MAP_HEIGHT/2);
 			win_pos.x = (map_pos.x * MINIMAP_SQUARE_SIZE);
 			win_pos.y = (map_pos.y * MINIMAP_SQUARE_SIZE);
-            if (map[map_pos.y][map_pos.x] != '0')
+			if (map[map_pos.y][map_pos.x] != '0')
 				draw_fill_square(img, win_pos, MINIMAP_SQUARE_SIZE, WALL_COLOR);
 			map_pos.x++;
 		}
@@ -56,11 +55,11 @@ static void render_background(t_img img)
 	int	j;
 
 	//i = (WIN_WIDTH / 2) - (MINIMAP_SQUARE_SIZE * MAP_WIDTH/2);
-    i = 0;
+	i = 0;
 	while (i < MINIMAP_SQUARE_SIZE * MAP_HEIGHT)
 	{
 		//j = (WIN_HEIGHT / 2) - (MINIMAP_SQUARE_SIZE * MAP_HEIGHT/2);
-        j = 0;
+		j = 0;
 		while (j < MINIMAP_SQUARE_SIZE * MAP_WIDTH)
 		{
 			draw_pixel(img, j, i, BGD_MINIMAP_COLOR);
@@ -75,16 +74,16 @@ static void render_player(t_img img, t_player player, char **map)
 	int		i;
 	t_pos	p_pos;
 	t_pos	ray_end_pos;
-    float   resize_scale;
+	float   resize_scale;
 
-    // square size (WIN_WIDTH / MAP_WIDT) tem de ser alterado para um valor constante
-    resize_scale = 2.66;//WIN_WIDTH / MAP_WIDTH / MINIMAP_SQUARE_SIZE;
+	// square size (WIN_WIDTH / MAP_WIDT) tem de ser alterado para um valor constante
+	resize_scale = 2.66;//WIN_WIDTH / MAP_WIDTH / MINIMAP_SQUARE_SIZE;
 
 	p_pos.x = player.pos.x - PLAYER_MINIMAP_SIZE / 2;
 	p_pos.y = player.pos.y - PLAYER_MINIMAP_SIZE / 2;
 
-    p_pos.x /= resize_scale;
-    p_pos.y /= resize_scale;
+	p_pos.x /= resize_scale;
+	p_pos.y /= resize_scale;
 
 	draw_fill_square(img, p_pos, PLAYER_MINIMAP_SIZE, PLAYER_COLOR);
 	i = 0;
@@ -96,32 +95,32 @@ static void render_player(t_img img, t_player player, char **map)
 	//}
 }
 
-static void render_player_circle(t_img img, t_player player, char **map)
+static void	render_player_circle(t_img img, t_player player, char **map)
 {
 	t_pos	p_pos;
 	t_pos	point_pos;
-	float 	i;
+	float	i;
+	float	resize_scale;	
 
-    float   resize_scale;
+	// square size (WIN_WIDTH / MAP_WIDT) tem de ser alterado para um valor constante
+	resize_scale = (float) WIN_WIDTH / MAP_WIDTH / MINIMAP_SQUARE_SIZE;
 
-    // square size (WIN_WIDTH / MAP_WIDT) tem de ser alterado para um valor constante
-    resize_scale = (float) WIN_WIDTH / MAP_WIDTH / MINIMAP_SQUARE_SIZE;
-
-    //printf("valor: %0.5f\n", resize_scale);
+	//printf("valor: %0.5f\n", resize_scale);
 	p_pos = player.pos;
 
-    //p_pos.x = player.pos.x - PLAYER_RADIUS / 2;
+	//p_pos.x = player.pos.x - PLAYER_RADIUS / 2;
 	//p_pos.y = player.pos.y - PLAYER_RADIUS / 2;
 
-    p_pos.x = p_pos.x / resize_scale;
-    p_pos.y = p_pos.y / resize_scale;
+	p_pos.x = p_pos.x / resize_scale;
+	p_pos.y = p_pos.y / resize_scale;
 
 	i = 0;
 	while (i <= 360)
 	{
 		point_pos = get_new_dist_pos(p_pos, i, 100);
 		//draw_pixel(img, point_pos.x, point_pos.y, PLAYER_COLOR);
-        draw_line_1(img, p_pos, point_pos, map, abs(player.dir + 90));
+		draw_line_1(img, p_pos, point_pos, map, i);
+		// draw_line_1(img, p_pos, point_pos, map, abs(player.dir + 90));
 
 		i += 0.01;
 		//i++;
@@ -153,7 +152,7 @@ static t_pos get_map_pos_1(t_pos pos)
 static unsigned int get_pixel_map_color(int x, int y, char **map)
 {
 	t_pos pos;
-	
+
 	pos.x = x;
 	pos.y = y;
 	pos = get_map_pos_1(pos);
@@ -200,12 +199,11 @@ static void draw_line_1(t_img img, t_pos init, t_pos end, char **map, float angl
 		//a = 10 * 20 /*cos_degree(1.9876)*/ + 10;
 		//a = 10 * 30/*sin_degree(1.9876)*/ + 10;
 
-		new_pos = transform_pos(init, pos, angle);
-		draw_pixel_1(img, new_pos.x, new_pos.y, get_pixel_map_color(pos.x, pos.y, map), init);
-		//draw_pixel_1(img, pos.x, pos.y, get_pixel_map_color(pos.x, pos.y, map), init);
+		//new_pos = transform_pos(init, pos, angle);
+		//draw_pixel_1(img, new_pos.x, new_pos.y, get_pixel_map_color(pos.x, pos.y, map), init);
+		draw_pixel_1(img, pos.x, pos.y, get_pixel_map_color(pos.x, pos.y, map), init);
 		pos.x += inc.x;
 		pos.y += inc.y;
 	}
 	draw_pixel_1(img, end.x, end.y, get_pixel_map_color(end.x, end.y, map), init);
 }
-
