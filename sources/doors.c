a29a60e0cc8ccdcaed5c4affe2f954dba3c671e5
Diogo andre Candeias antunes <dcandeia@c2r10s8.42lisboa.com>
2023-02-28 11:52:01 +0000

    Fixed bug in open and close doors

--- sources/doors.c
+++ sources/doors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   doors.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dcandeia < dcandeia@student.42lisboa.co    +#+  +:+       +#+        */
+/*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:54:55 by dcandeia          #+#    #+#             */
-/*   Updated: 2023/02/24 16:23:19 by dcandeia         ###   ########.fr       */
+/*   Updated: 2023/02/28 11:50:48 by dcandeia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #define TRUE	1
 #define FALSE	0
 
-static int	door_check_interaction(t_ray ray, char **map);
+static int	door_check_interaction(t_ray ray, char **map, t_player *player);
 static char	door_status_update(char old_status);
 
 void	doors_interaction(t_map map, t_player *player)
@@ -24,7 +24,7 @@ void	doors_interaction(t_map map, t_player *player)
 	t_pos	door;
 
 	view = player->rays[NUMBER_RAYS / 2];
-	if (!door_check_interaction(view, map.content))
+	if (!door_check_interaction(view, map.content, player))
 		return ;
 	door = view.door_pos;
 	if (map.content[door.y][door.x] == D_FRAME_OPEN_0)
@@ -56,13 +56,21 @@ void	doors_control(t_map map)
 	}
 }
 
-static int	door_check_interaction(t_ray ray, char **map)
+static int	door_check_interaction(t_ray ray, char **map, t_player *player)
 {
+	t_pos		p_map_pos;
+
+	p_map_pos = get_map_pos(player->pos);
 	if (!ray.is_door)
 		return (FALSE);
-	else if (ray.door_dist > DIST_OPEN_DOOR)
+	else if (ray.door_dist > DIST_OPEN_DOOR || ray.door_dist < 0.10)
 		return (FALSE);
 	else if (ray.door_pos.x < 0 || ray.door_pos.y < 0)
+		return (FALSE);
+	else if ((map[p_map_pos.y][p_map_pos.x] >= 'G'
+		&& map[p_map_pos.y][p_map_pos.x] <= 'N')
+		|| (map[p_map_pos.y][p_map_pos.x] >= 'g'
+		&& map[p_map_pos.y][p_map_pos.x] <= 'n'))
 		return (FALSE);
 	return (TRUE);
 }
